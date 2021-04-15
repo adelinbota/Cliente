05bc4be2d893e8b2f0efbaca4d59e334926ca2a2
Adelin <Adebota02@gmail.com>
2021-04-15 10:20:06 +0200

    IODatos

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -11,4 +11,63 @@ package Controlador;
  */
 public class Controlador {
     
+        public static void encriptar(ArrayList<Cliente> vDatos){
+        
+        String datos= "clientes.dat";
+        File f= new File(datos);
+    
+            if(!f.exists()){
+            try {
+                f.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            }
+
+            
+        try (FileOutputStream fo = new FileOutputStream(f); 
+                ObjectOutputStream escribir = new ObjectOutputStream (fo)){
+            
+                escribir.writeObject(vDatos);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+      public static ArrayList<Cliente> desencriptar(){
+        
+         ArrayList <Cliente> vDatos = new ArrayList();
+        String datos= "clientes.dat";
+        File f= new File(datos);
+    
+         if(!f.exists()){
+            try {
+                f.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            }
+
+            
+        try (FileInputStream fi = new FileInputStream(f); 
+                ObjectInputStream leer = new ObjectInputStream (fi)){
+            
+                vDatos = (ArrayList<Cliente>) leer.readObject();
+          
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("1");           
+// Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Fichero creado de 0");
+// Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+System.out.println("3");
+            // Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return vDatos;
+      }
 }
